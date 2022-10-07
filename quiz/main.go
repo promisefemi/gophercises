b80f06222c94f295f0c89cdd8aaf3798f331f86a
Promise Femi <promisefemi90@gmail.com>
2022-10-07 19:43:55 +0100

    Working on Quiz

--- quiz/main.go
+++ quiz/main.go
@@ -1,6 +1,30 @@
 package main
-import("fmt")
 
-func main(){
-	fmt.Println("...Loading Files...")
+import (
+	"flag"
+	"fmt"
+	"io"
+	"os"
+)
+
+func main() {
+	csv := flag.String("csv", "problems.csv", "A csv file to import")
+	flag.Parse()
+
+	file, err := os.Open(*csv)
+	if err != nil {
+		exit(fmt.Sprintf("Unable to open file %s", *csv))
+	}
+
+	body, err := io.ReadAll(file)
+	if err != nil {
+		exit("Unfortunately something went wrong")
+	}
+
+	fmt.Printf("%s", body)
+}
+
+func exit(message string) {
+	fmt.Println(message)
+	os.Exit(1)
 }
