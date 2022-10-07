f7cdefb2ce4a2b5d9b5eb8947495235f6b8f4026
Promise Femi <promisefemi90@gmail.com>
2022-10-07 21:01:22 +0100

    Update on the quiz

--- quiz/main.go
+++ quiz/main.go
@@ -1,6 +1,33 @@
 package main
-import("fmt")
 
-func main(){
-	fmt.Println("...Loading Files...")
+import (
+	"encoding/csv"
+	"flag"
+	"fmt"
+	"os"
+)
+
+func main() {
+	csvFileName := flag.String("csv", "problems.csv", "A CSV file in  the format of 'question, answer'")
+	flag.Parse()
+
+	file, err := os.Open(*csvFileName)
+	if err != nil {
+		exit(fmt.Sprintf("Failed to open the CSV file: %s\n", *csvFileName))
+	}
+
+	r := csv.NewReader(file)
+
+	lines, err := r.ReadAll()
+	if err != nil {
+		exit("Failed to parse the provided CSV")
+	}
+
+	fmt.Printf("%s", lines)
+
+}
+
+func exit(msg string) {
+	fmt.Println(msg)
+	os.Exit(1)
 }
