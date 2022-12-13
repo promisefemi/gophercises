8cf93baffa17458c8edbdc3f52e7ad93dac13c90
Promise Femi <promisefemi90@gmail.com>
2022-12-13 16:28:02 +0100

    Completion of CYOA

--- cyoa/cmd/cyoa/main.go
+++ cyoa/cmd/cyoa/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 	"os"
@@ -25,9 +26,24 @@ func main() {
 		panic(err)
 	}
 	// fmt.Printf("%+v\n", &story)
-	h := cyoa.NewHandler(story, nil)
+	templ := template.Must(template.New("").Parse(cyoa.ParseTemplateHTML("cmd/cyoa/cyoastoryhtml.html")))
+	h := cyoa.NewHandler(story,
+		cyoa.WithPathFunc(pathFn),
+		cyoa.WithTemplate(templ),
+	)
 
 	fmt.Printf(" App runninng on http://localhost:%d\n", *port)
 
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), h))
+	mux := http.NewServeMux()
+	mux.Handle("/story/", h)
+
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), mux))
+}
+
+func pathFn(r *http.Request) string {
+	path := r.URL.Path
+	if path == "/story" || path == "/story/" {
+		path = "/story/intro"
+	}
+	return path[len("/story/"):]
 }
--- cyoa/story.go
+++ cyoa/story.go
@@ -6,37 +6,38 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"os"
 )
 
-var defaultHandlerTempl = `
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Document</title>
-  </head>
-  <body>
-    <h1>{{.Title}}</h1>
-    {{range .Paragraphs}}
-    <p>{{.}}</p>
-    {{end}}
-    <ul>
-      {{range .Options}}
-      <li>
-        <a href="/{{.Chapter}}">{{.Text}}</a>
-      </li>
-      {{end}}
-    </ul>
-  </body>
-</html>
+// var defaultHandlerTempl = `
+// <!DOCTYPE html>
+// <html lang="en">
+//   <head>
+//     <meta charset="UTF-8" />
+//     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+//     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+//     <title>Document</title>
+//   </head>
+//   <body>
+//     <h1>{{.Title}}</h1>
+//     {{range .Paragraphs}}
+//     <p>{{.}}</p>
+//     {{end}}
+//     <ul>
+//       {{range .Options}}
+//       <li>
+//         <a href="/{{.Chapter}}">{{.Text}}</a>
+//       </li>
+//       {{end}}
+//     </ul>
+//   </body>
+// </html>
 
-`
+// `
 var templ *template.Template
 
 func init() {
-	templ = template.Must(template.New("").Parse(defaultHandlerTempl))
+	templ = template.Must(template.New("").Parse(ParseTemplateHTML("cmd/cyoa/cyoahtml.html")))
 }
 
 type Story map[string]Chapter
@@ -55,6 +56,15 @@ type Demo struct {
 	Age  int    `json:"age,omitempty"`
 }
 
+func ParseTemplateHTML(filePath string) string {
+	defaultTemplateFile, err := os.ReadFile(filePath)
+
+	if err != nil {
+		panic(err)
+	}
+	return string(defaultTemplateFile)
+}
+
 func JSONStory(fileReader io.Reader) (Story, error) {
 
 	d := json.NewDecoder(fileReader)
@@ -62,34 +72,51 @@ func JSONStory(fileReader io.Reader) (Story, error) {
 	if err := d.Decode(&story); err != nil {
 		return nil, err
 	}
-
 	return story, nil
 }
 
-func NewHandler(s Story, tmpl *template.Template) http.Handler {
-	t := templ
-	if tmpl != nil {
-		t = tmpl
-	}
-
-	return handler{s, t}
-}
-
 type handler struct {
-	s Story
-	t *template.Template
+	s      Story
+	t      *template.Template
+	pathFn func(r *http.Request) string
 }
 
-func (h handler) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
+// Configurations for Handlers
+type HandlerOption func(h *handler)
+
+func WithTemplate(t *template.Template) HandlerOption {
+	return func(h *handler) {
+		h.t = t
+	}
+}
+
+func WithPathFunc(fn func(r *http.Request) string) HandlerOption {
+	return func(h *handler) {
+		h.pathFn = fn
+	}
+}
+
+func NewHandler(s Story, options ...HandlerOption) http.Handler {
+	h := handler{s, templ, defaultPathFn}
+
+	for _, opt := range options {
+		opt(&h)
+	}
+	return h
+}
+
+func defaultPathFn(r *http.Request) string {
 	path := r.URL.Path
 
 	if path == "" || path == "/" {
 		path = "/intro"
 	}
-	path = path[1:]
-
+	return path[1:]
+}
+func (h handler) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
+	path := h.pathFn(r)
 	if chapter, ok := h.s[path]; ok {
-		err := templ.Execute(rw, chapter)
+		err := h.t.Execute(rw, chapter)
 		if err != nil {
 			log.Printf("%v", err)
 			http.Error(rw, "Something went wrong...", http.StatusInternalServerError)
